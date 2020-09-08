a3ae97c6ab8aabf32901988ca7a4e8c65a16fd1d
bingyue.wang <odowong@hotmail.com>
2020-09-07 22:29:50 -0400

    added firebase config file and initial react app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-24llecLn2JfdDfWGbenD138pVhK6buQ",
+    authDomain: "chatapp-cf2dc.firebaseapp.com",
+    databaseURL: "https://chatapp-cf2dc.firebaseio.com",
+    projectId: "chatapp-cf2dc",
+    storageBucket: "chatapp-cf2dc.appspot.com",
+    messagingSenderId: "518046426990",
+    appId: "1:518046426990:web:05d244c27eea1016ab437f",
+    measurementId: "G-99FLWT64P5"
+};
