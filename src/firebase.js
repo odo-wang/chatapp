8d8e08e3ec51773daab3c7236ae361f532b3d83b
bingyue.wang <odowong@hotmail.com>
2020-09-08 22:14:44 -0400

    configure the firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyC-24llecLn2JfdDfWGbenD138pVhK6buQ",
     authDomain: "chatapp-cf2dc.firebaseapp.com",
@@ -6,5 +8,13 @@ const firebaseConfig = {
     storageBucket: "chatapp-cf2dc.appspot.com",
     messagingSenderId: "518046426990",
     appId: "1:518046426990:web:05d244c27eea1016ab437f",
-    measurementId: "G-99FLWT64P5"
+    measurementId: "G-99FLWT64P5",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebaseApp.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export {auth, provider}
+export default db
