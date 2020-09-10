3ca6b495a0ae7b564b4d2e0b57f9fbc53fe9f050
bingyue.wang <odowong@hotmail.com>
2020-09-09 21:55:41 -0400

    bug fix, adding id to deps in SidebarChat.js

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -21,7 +21,7 @@ function SidebarChat({id, name, addNewChat}) {
                     setMessages(snapshot.docs.map(doc => doc.data()))
                 ))
         }
-    }, [])
+    }, [id])
 
     function createChat() {
         const roomName = prompt("Enter name for chat room")
